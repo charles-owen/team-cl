4f2207d59796cefeb67100569ae284a5016eab31
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:07:01 -0500

    New cl-publish system.

--- webpack.common.js
+++ webpack.common.js
@@ -4,14 +4,5 @@ const FileManagerPlugin = require('filemanager-webpack-plugin');
 module.exports = {
 	entry: {
 		Team: path.resolve(__dirname, 'index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/team.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
